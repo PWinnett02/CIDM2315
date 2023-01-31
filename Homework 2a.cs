8153c9051d5e5d5ec199f2ac16ba39a6dd5c127f
PWinnett02 <123031103+PWinnett02@users.noreply.github.com>
2023-01-30 19:52:52 -0600

    Add files via upload

---
+++ Homework 2a.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Please input a letter grade:");
+        //get grade from user
+        char grade=Console.ReadLine()[0];
+        //switch block
+        switch (grade)
+        {
+            //Check if grade is A
+            //if A then print 4
+            case 'A': Console.WriteLine("GPA Point: 4");
+            break;
+            //check if grade is B
+            //if B then print 3
+            case 'B': Console.WriteLine("GPA Point: 3");
+            break;
+            //check if grade is C
+            //if C then print 2
+            case 'C': Console.WriteLine("GPA Point: 2");
+            break;
+            //check if grade is D
+            //if D then print 1
+            case 'D': Console.WriteLine("GPA Point: 1");
+            break;
+            //check if grade is F
+            //if F then print 0
+            case 'F': Console.WriteLine("GPA Point: 0");
+            break;
+            //if any other letter then A,B,C,D,F
+            default: Console.WriteLine("Wrong Letter Grade!")
+            break;
+        }
+    }
+}
+
+        
---
+++ Homework 2b.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+       //get three inputs from the user
+       Console.WriteLine("Please input the first num:");
+       int a=Convert.ToInt16(Console.ReadLine());
+       Console.WriteLine("Please input the second num:");
+       int b=Convert.ToInt16(Console.ReadLine());
+       Console.WriteLine("Please input the third num:");
+       int c=Convert.ToInt16(Console.ReadLine());
+       //declare a variable smallest
+       int smallest=0;
+       //check if a is the smallest 
+       if (a<b)
+       {
+        if (a<c) 
+        {
+            smallest=a;
+        }
+       }
+       //check if b is the smallest
+       if (b<a)
+       {
+        if (b<c)
+        {
+            smallest=b;
+        }
+       }
+       //check if c is the smallest
+       if (c<a)
+       {
+        if (c<b)
+        {
+            smallest=c;
+        }
+       }
+        //print the smallest
+        Console.WriteLine("The smallest Value is {0}",smallest);
+    }
+}
+
+        
