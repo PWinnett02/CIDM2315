6ea0156cbf8e7a45769a75de62009cc258438be4
PWinnett02 <123031103+PWinnett02@users.noreply.github.com>
2023-02-06 16:50:41 -0600

    Add files via upload

---
+++ Homework 3a.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Assign an int value to N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i=0;i<number;i++);
+        {
+            for(int j=0<number;j++);
+            {
+                Console.WriteLine("#");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Homework 3b.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Pattern
+{
+    static void Main()
+    {
+        Console.WriteLine("Assign an int value to N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i=0; i<n; i++);
+        {
+            for(int j=0; j<n; j++);
+            {
+                Console.WriteLine("#");
+            }
+            Console.WriteLine();
+        }
+    }
+}
