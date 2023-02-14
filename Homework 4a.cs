e52b882bcbf1d7cbd5aadbd0e0f5aba099be07a3
PWinnett02 <123031103+PWinnett02@users.noreply.github.com>
2023-02-13 19:26:55 -0600

    Add files via upload

---
+++ Homework 4a.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class HelloWorld
+{
+   //function named largest that takes 2 integers a and b
+   //and return largest one
+   static int largest(int a, int b)
+   {
+    if(a>b)
+    {
+        return a;
+    }
+    else if(b>a)
+    {
+        return b;
+    }
+    else
+    {
+      return -1;  
+    }
+    }
+    //main function
+    static void Main()
+    {
+        int a=3,b=5;
+        //call the largest function in main function
+        int lar = largest(a,b);
+        //and print it
+        Console.WriteLine("\nLargest Number is:"+lar);
+    }
+   }
---
+++ homework 4b.cs
@@ -0,0 +1,61 @@
+﻿using System;
+class HelloWorld
+{
+    static void pattern(int N,string shape)
+    {
+        //pattern method having parameters N and shape
+        int i,j,k;
+        //declaring variables to use 
+        if(shape=="left")
+        {
+            {
+                //if shape is left
+                for(i=1;i<=N;i++)
+                {
+                    for(j=1;j<=N;j++)
+                    Console.WriteLine("*");
+                    //printing star
+                    Console.WriteLine("\n");
+                    //printing new line
+                }
+            }
+        else if(shape== "right")
+        {
+            //shape is right
+            for(i=1;i<=N;i++)
+            {
+                for(j=1;j<=N;j++)
+                {
+                    Console.WriteLine(" ");
+                }
+                //printing space
+                for(k=1;k<=N;k++)
+                {
+                    Console.WriteLine("*");
+                }
+                //printing star
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+    static void Main()
+    {
+        //main method
+        int N;
+        //declaring variable
+        string shape;
+        //declaring the shape
+        Console.WriteLine("Input N: ");
+        N= Convert.ToInt32(Console.ReadLine());
+        //input the number of rows
+        Console.WriteLine("Input shape: ");
+        shape= Console.ReadLine();
+        //input of the shape
+        Console.WriteLine("N is: "+N+"; shape is "+shape+"\n");
+        //displaying N and shape pattern(N,shape);
+        //calling method
+    }
+}
+}
+
